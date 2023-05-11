60855c4de00dd1519938b79b3ad1f82c122e49be
freetimezp <kraftukrnet@ukr.net>
2023-05-11 17:48:38 +0300

    02. js-eatme-06-foodio. Home section. Swiper slider.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,19 @@
 /*=============== REMOVE MENU MOBILE ===============*/
 
 /*=============== HOME SWIPER ===============*/
-
+var homeSwiper = new Swiper('.home-swiper', {
+    spaceBetween: 30,
+    grubCursor: true,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+});
 /*=============== DEALS TAB ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/
