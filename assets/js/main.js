a79a857f950954d9ea74ed3c5a287705477a0d2f
freetimezp <kraftukrnet@ukr.net>
2023-05-11 15:01:36 +0300

    01. js-eatme-06-foodio. Start.

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== HOME SWIPER ===============*/
+
+/*=============== DEALS TAB ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
