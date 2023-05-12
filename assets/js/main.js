0cc8b3d6374fd04a9fe57e3729cc1541d625ee44
freetimezp <kraftukrnet@ukr.net>
2023-05-12 20:42:58 +0300

    07. js-eatme-06-foodio. Deals section. Tabs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,25 @@ var homeSwiper = new Swiper('.home-swiper', {
         prevEl: '.swiper-button-prev'
     }
 });
+
 /*=============== DEALS TAB ===============*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[content]');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('active-tab');
+        });
+
+        target.classList.add('active-tab');
+        tabs.forEach((tab) => {
+            tab.classList.remove('active-tab');
+        });
+        tab.classList.add('active-tab');
+    });
+});
 
 /*=============== SHOW SCROLL UP ===============*/
