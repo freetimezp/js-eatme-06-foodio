fae101136988c1ff09fcbab87fbf2676029c196d
freetimezp <kraftukrnet@ukr.net>
2023-05-13 18:08:36 +0300

    11. js-eatme-06-foodio. Scrollup btn.

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,14 @@ tabs.forEach((tab) => {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollup = document.getElementById('scroll-up');
+
+    if (this.scrollY >= 300) {
+        scrollup.classList.add('show-scroll');
+    } else {
+        scrollup.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
